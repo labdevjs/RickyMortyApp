4926089c54873a10df876d8bd7db6f073390e443
labdevjs <labdevjs@gmail.com>
2021-03-25 07:33:00 +0000

    24

---
+++ src/app/services/characters.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { APIURL } from '../../environments/R&MAPI'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharactersService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getCharacters() {
+    return this.http.get(APIURL.characters);
+  }
+}
